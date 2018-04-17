fa3df5cf46af772dd3939124e616a67ce7af45d3
zushimaru <a_kanda@gmail.com>
2018-04-17 23:57:28 +0900

    [mod] 標準入力に修正

--- chk_zorome.sh
+++ chk_zorome.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 IFS=','
-hoge='123,abc,3333,555,321'
-for i in $hoge; do
+echo -n ">"
+read INPUT
+for i in $INPUT; do
   if echo "$i" | grep -E "^([0-9])\1{2}$" >/dev/null 2>&1; then
         echo "zorome: $i"
   elif [[ $i =~ ^[0-9]{3}$ ]]; then
