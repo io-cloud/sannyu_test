fc46edfd5ba620bfa398da13af9de8a3b65e37d8
Shika-kura <46592630+Shika-kura@users.noreply.github.com>
2019-01-17 09:29:14 +0900

    Update chk_zorome.sh

--- chk_zorome.sh
+++ chk_zorome.sh
@@ -2,12 +2,11 @@
 
 VAR=`echo $1 | tr ',' '\n'`
 for i in $VAR ;do
-                if echo "$i" | grep -E "([0-9])\1{2}" > /dev/null ; then
-                        echo "zorome:$i"
-                elif [[ "$i" =~ [0-9]{3} ]]; then
-                        echo "$i"
-                else
-                        echo "unmatch pattern:$i"
-
-                fi
+                        if echo "$i" | grep -E "(^[0-9])\1{2}$" > /dev/null ; then
+                                        echo "zorome:$i"
+                        elif [[ "$i" =~ ^[0-9]{3}$ ]]; then
+                                        echo "$i"
+                        else
+                                        echo "unmatch pattern:$i"
+                        fi
 done
