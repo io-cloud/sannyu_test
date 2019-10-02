9f894d327061f20f943c62ea5230096e0308b012
nefer <c01121849e@edu.teu.ac.jp>
2019-10-02 18:50:38 +0900

    [MOD]設問10-1に解答し、bak.shのIS_RESTORE判定部分を修正
    本来であれば-r(リストア)の際に、バックアップファイルからのリストアを
    行うべき、であれば本if文の条件判定は-rが指定されたかどうかであるはずだが、
    ここでは-bが指定されたかどうかを判定してしまっていたため、修正
    本格的なリファクタリングを加えるのであれば、挙動が真逆のバックアップ処理とリストア処理は
    関数化し分割しておけば、IS_RESTOREフラグの混乱などが起きにくくなり、
    このようなバグは起きにくくなると思われる

--- bak.sh
+++ bak.sh
@@ -31,7 +31,15 @@ shift $((OPTIND - 1))
 
 for file in $*
 do
-    if [ "$IS_RESTORE" -eq 0 ]; then
+### Exam.10-1 MOD ###
+# 本来であれば-r(リストア)の際に、バックアップファイルからのリストアを
+# 行うべき、であれば本if文の条件判定は-rが指定されたかどうかであるはずだが、
+# ここでは-bが指定されたかどうかを判定してしまっていたため、修正
+# 本格的なリファクタリングを加えるのであれば、挙動が真逆のバックアップ処理とリストア処理は
+# 関数化し分割しておけば、IS_RESTOREフラグの混乱などが起きにくくなり、
+# このようなバグは起きにくくなると思われる
+    if [ "$IS_RESTORE" -eq 1 ]; then
+### END ###
         if [ ! -e "${file}.bak" ]; then
             echo "ERROR: A file doesn't exist. ${file}.bak"
             continue
