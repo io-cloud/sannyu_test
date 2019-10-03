c6ef31b6eef8729855074479fb89acb58ed6c6bb
nefer <c01121849e@edu.teu.ac.jp>
2019-10-04 07:31:15 +0900

    [MOD]設問9の解答となるchk_zorome.shを修正
    問題の読み違えにより不正な挙動をしていた、chk_zorome.shを修正

--- chk_zorome.sh
+++ chk_zorome.sh
@@ -1,12 +1,20 @@
 #! /bin/bash
 str=$1
 
-if test -z "`echo $str | grep -E '^[0-9]{3}$'`" ; then
-  echo "unmach pattern: ${str}"
+if test -n `echo $str | grep -E ','` ; then
+	strs=`echo $str | sed -e 's/,/\n/g'`
 else
-  if test "${str:0:1}" = "${str:1:1}" -a "${str:1:1} = "${str:2:1} ; then
-    echo "zorome: ${str}"
-  else
-    echo ${str}
-  fi
+	strs=`echo $str | sed -e 's/ /\n/g'`
 fi
+
+for str in `echo $strs` ; do
+	if test -z "`echo $str | grep -E '^[0-9]{3}$'`" ; then
+	  echo "unmach pattern: ${str}"
+	else
+	  if test "${str:0:1}" = "${str:1:1}" -a "${str:1:1} = "${str:2:1} ; then
+	    echo "zorome: ${str}"
+	  else
+	    echo ${str}
+	  fi
+	fi
+done
