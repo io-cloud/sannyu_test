fa89e9e1524daf4f6fd740c3ea88524fcc741d24
nishi86san <nishi86ritts@gmail.com>
2022-01-24 10:56:58 +0900

    Update bak.sh

--- bak.sh
+++ bak.sh
@@ -31,7 +31,7 @@ shift $((OPTIND - 1))
 
 for file in $*
 do
-    if [ "$IS_RESTORE" -eq  ]; then
+    if [ "$IS_RESTORE" -eq 1 ]; then
         if [ ! -e "${file}.bak" ]; then
             echo "ERROR: A file doesn't exist. ${file}.bak"
             continue
